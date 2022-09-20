3bb87e8f4280bdcde51bdc11e6838a7bb0806d9b
Jacob Sanford <jsanford@unb.ca>
2022-09-20 15:16:48 -0300

    Initial prototype commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dockworker-cli
+Extends the base dockworker framework (https://github.com/unb-libraries/dockworker) to provide additional functionality for CLI driven applications.
---
+++ src/Robo/Plugin/Commands/DockworkerCliCommands.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dockworker\Robo\Plugin\Commands;
+
+use Dockworker\Robo\Plugin\Commands\DockworkerBaseCommands;
+
+/**
+ * Defines a base class for all Dockworker-CLI Robo commands.
+ */
+class DockworkerCliCommands extends DockworkerBaseCommands {
+
+}
