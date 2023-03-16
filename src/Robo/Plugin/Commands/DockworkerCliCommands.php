4768c4ae8c516c8345ad42d1b5f594ef9c01d5c4
Jacob Sanford <jsanford@unb.ca>
2023-03-16 12:02:59 -0300

    Whitespace, code standards

--- src/Robo/Plugin/Commands/DockworkerCliCommands.php
+++ src/Robo/Plugin/Commands/DockworkerCliCommands.php
@@ -5,7 +5,7 @@ namespace Dockworker\Robo\Plugin\Commands;
 use Dockworker\DockworkerCommands;
 
 /**
- * Defines a base class for all Dockworker-CLI Robo commands.
+ * Defines a base class for all Dockworker-CLI commands.
  */
 class DockworkerCliCommands extends DockworkerCommands {
 
